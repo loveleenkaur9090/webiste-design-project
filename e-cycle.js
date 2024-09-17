210926166e1bd1622527b7dec651e64dcbe99d3e
Loveleen <kamaljeetpaul9107@gmail.com>
2024-09-17 19:38:17 +0530

    ok done

---
+++ e-cycle.js
@@ -0,0 +1,14 @@
+let ArrProducts=[
+    {
+        id:1,
+        name:"EP-2 Pro/E-bike",
+        image:"img/bike/cycle.png",
+        paragrap:"120 km Range Max Speed 25 km/h Front Suspension Foldable",
+    },
+    {
+        id:2,
+        name:"The Power Of Your Subconcious Mind",
+        image:"img-2.jfif",
+        paragrap:"120 km Range Max Speed 25 km/h Front Suspension Foldable",
+    },
+    ]; 
